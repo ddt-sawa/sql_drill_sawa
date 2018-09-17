6541354015b3773ec8abf217893de12fa125d420
ddt-sawa <ddt.sawa@gmail.com>
2018-09-17 23:56:19 +0900

    add text 4-4

---
+++ 4-4_question1.sql
@@ -0,0 +1,17 @@
+/*4-4 問1 書籍テーブル(books)上、publish列の定義(VARCHAR(100),NULLを許可)で変更してみましょう。
+以下の空欄を埋めて、SQL命令を完成させてください。*/
+
+--変更を加えるテーブル
+ALTER TABLE
+
+	--書籍テーブル
+	books
+	
+--列の定義を変更
+ALTER COLUMN
+
+	--publish列の定義(VARCHAR(100),NULLを許可)
+	publish TYPE VARCHAR(100)
+;
+
+
---
+++ 4-4_question4.sql
@@ -0,0 +1,32 @@
+/*4-4 問4 以下は、アクセス記録テーブル(access_log)に対して、既存のreferer列の定義を
+「データ型VARCHAR(200)、NULL値を許可」で置き換えるためのSQL命令ですが、
+2点誤りがあります。誤りを指摘してください。*/
+
+--変更を加えるテーブル
+ALTER TABLE
+
+	--アクセス記録テーブル
+	access_log
+	
+	
+--①WITHは不要
+--MODIFY WITH
+
+--MODIFY
+	--②カンマは不要
+	--referer,VARCHAR(100),NULL
+	
+	--以下、PostgresSQLでの表記
+	
+--列の定義を変更
+ALTER COLUMN
+
+	--参照元URLのデータ型をVARCHAR(200)に変更
+	referer TYPE VARCHAR(200),
+	
+--列の定義を変更
+ALTER COLUMN
+
+	--参照元URLのNULL値を許可
+	referer DROP NOT NULL
+;
