615a02cf193a7e52e5a558f1a10d9747e5938d23
ddt-sawa <ddt.sawa@gmail.com>
2018-09-20 21:17:56 +0900

    mod text, Chapter3

---
+++ Chapter3/Chapter03-06/Chapter03-06-04.sql
@@ -0,0 +1,50 @@
+/*3-6 問4 貸出記録テーブル(rental)上、returned列が9()紛失であるレコードについて、
+対応する書籍情報テーブル(books)上の分類IDを「XXXXX」（その他）に更新してみましょう。*/
+
+--更新対象
+UPDATE
+
+	--書籍情報テーブル
+	books
+
+--更新内容	
+SET
+	
+	--分類IDを「XXXXX」（その他）に更新
+	category_id = 'XXXXX'
+	
+--更新条件
+WHERE
+
+	--サブクエリが真である場合
+	EXISTS
+	
+		--貸出記録テーブルで書籍が紛失中であるかどうかを調べるサブクエリ
+		(
+		
+			--取得列
+			SELECT
+			
+				--全列
+				*
+			
+			--参照範囲	
+			FROM
+			
+				--貸出記録テーブル
+				rental
+			
+			--取得条件	
+			WHERE
+			
+				--書籍テーブルのisbnと貸出テーブルのisbnが一致
+				books.isbn = rental.isbn
+			
+			--かつ
+			AND
+			
+				--紛失中
+				returned = 9
+			
+		)
+;
---
+++ Chapter3/Chapter03-07/Chapter03-07-04.sql
@@ -0,0 +1,28 @@
+/*3-7 問4 以下は、書籍情報テーブル(books)上で分類IDが「Z9999(未分類)」であるか、NULLである書籍を削除する
+SQL命令ですが、誤りが2点あります。誤りを指摘してください。*/
+
+①--FROMがない
+--DELETE
+
+--削除対象
+DELETE FROM
+
+ 	--書籍情報テーブル
+	books
+	
+--削除条件
+WHERE
+
+	--分類IDが「Z9999(未分類)
+	category_id = 'Z9999'
+	
+--②ANDではなくOR
+--AND
+
+--または
+OR
+
+	--分類IDがNULL
+	category_id IS NULL 
+;
+	
