e522a6e46bca3f626e97a881d36552a282f359fc
ddt-sawa <ddt.sawa@gmail.com>
2018-10-03 20:08:14 +0900

    mod text, 01-03 結果出力

--- Chapter1/Chapter01-03/Chapter01-03-02.sql
+++ Chapter1/Chapter01-03/Chapter01-03-02.sql
@@ -1,4 +1,4 @@
-/*1-3 問2 アクセス記録テーブル(access_log)からリンク元URLを重複のない形式で取り出してみましょう。
+﻿/*1-3 問2 アクセス記録テーブル(access_log)からリンク元URLを重複のない形式で取り出してみましょう。
 以下の空欄を埋めて、SQL命令を完成させてください。*/
 
 --取得列(重複なし)
@@ -13,3 +13,15 @@ FROM
 	--アクセス記録テーブル
 	access_log
 ;
+
+/*
+          referer
+---------------------------
+ http://wings.msn.to/hamu/
+
+ http://wings.msn.to/kame/
+ http://wings.msn.to/tori/
+ http://wings.msn.to/inu/
+ http://wings.msn.to/neko/
+ http://wings.msn.to/saru/
+*/
--- Chapter1/Chapter01-03/Chapter01-03-04.sql
+++ Chapter1/Chapter01-03/Chapter01-03-04.sql
@@ -1,8 +1,5 @@
-/*1-3 問4 以下は、社員テーブル(employee)から重複なしの役職を取り出すためのSQL命令ですが、
+﻿/*1-3 問4 以下は、社員テーブル(employee)から重複なしの役職を取り出すためのSQL命令ですが、
 2点誤りがあります。誤りを指摘してください。*/
-
---①ALLではなくDISTINCT、②重複なし役職に部門IDは不要
---SELECT ①ALL class, ②depart_id FROM employee;
 
 --①ALLではなくDISTINCT
 --SELECT ALL 
@@ -22,3 +19,13 @@ FROM
 	--社員テーブル
 	employee
 ;
+
+/*
+    class
+--------------
+ アシスタント
+ 主任
+ 課長
+ 部長
+ 担当
+*/
