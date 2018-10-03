cf7934925496f24c26501abda7ef9b4dc121aa2a
ddt-sawa <ddt.sawa@gmail.com>
2018-10-03 20:12:03 +0900

    mod text, 01-04 結果出力

--- Chapter1/Chapter01-04/Chapter01-04-03.sql
+++ Chapter1/Chapter01-04/Chapter01-04-03.sql
@@ -1,11 +1,17 @@
-/*1-4 問3 ユーザテーブル(usr)から「東京都」に住んでいない人の情報だけを取り出してみましょう。
+﻿/*1-4 問3 ユーザテーブル(usr)から「東京都」に住んでいない人の情報だけを取り出してみましょう。
 取り出す列は、l_name、f_name、email列とします。*/
 
 --取得列
 SELECT 
 
-	--氏,名,メールアドレス
-	l_name, f_name, email 
+	--氏
+	l_name, 
+
+	--名
+	f_name,
+
+	--メールアドレス
+	email 
 	
 	
 --参照範囲
@@ -21,3 +27,26 @@ WHERE
 	--都道府県が東京都以外
 	prefecture <> '東京都'
 ;
+
+/*
+ l_name | f_name |        email
+--------+--------+----------------------
+ 井上   | 一郎   | inoue@wings.msn.to
+ 上原   | 幸一   | uehara@wings.msn.to
+ 江本   | 聡     | emoto@wings.msn.to
+ 河合   | 泰治   | kawai@wings.msn.to
+ 矢口   | 亜由美 | yaguchi@wings.msn.to
+ 有木   | 茉莉   | yuki@wings.msn.to
+ 吉岡   | 遥     | yoshi@examples.com
+ 松本   | 博美   | matsu@wings.msn.to
+ 木下   | 一樹   | kino@wings.msn.to
+ 久保田 | 守     | kubota@wings.msn.to
+ 児玉   | 毅     | kodama@wings.msn.to
+ 坂口   | 駿     | saka@examples.com
+ 井上   | 一郎   | shimi@wings.msn.to
+ 南     | 沙恵   | minami@wings.msn.to
+ 村井   | 佐知子 | murai@wings.msn.to
+ 森本   | 絵里   | morimo@examples.com
+ 西島   | 文子   | nishi@wings.msn.to
+ 根岸   | 裕子   | negi@wings.msn.to
+*/
