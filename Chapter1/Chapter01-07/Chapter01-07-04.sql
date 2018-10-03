8b64634b87d761ee1c098da99467b243311acfbf
ddt-sawa <ddt.sawa@gmail.com>
2018-10-03 20:20:10 +0900

    mod text, 01-07 結果出力

--- Chapter1/Chapter01-07/Chapter01-07-04.sql
+++ Chapter1/Chapter01-07/Chapter01-07-04.sql
@@ -1,4 +1,4 @@
-/*1-7 問4 以下は、アクセス記録テーブル(access_log)から2013年1月分のアクセスログをreferer,ip_address列について
+﻿/*1-7 問4 以下は、アクセス記録テーブル(access_log)から2013年1月分のアクセスログをreferer,ip_address列について
 取り出すためのSQL命令ですが、2点誤りがあります。誤りを指摘してください。*/
 
 --選択範囲
@@ -28,7 +28,26 @@ ORDER BY
 	--②referer列のソート方法が省略されているため昇順になってしまう
 	--referer, ip_address DESC
 	
-	--referer列とip_adress列について降順
-	referer DESC, ip_address DESC
+	--referer列について降順
+	referer DESC, 
+
+	--ipアドレスについて降順
+	ip_address DESC
 ;
 
+/*
+ log_id | page_id |          referer          |   ip_address    |     access_date
+--------+---------+---------------------------+-----------------+---------------------
+    116 | C0001   | http://wings.msn.to/tori/ | 192.168.15.201  | 2013-01-01 13:18:14
+    122 | C0003   | http://wings.msn.to/saru/ | 192.168.209.236 | 2013-01-03 14:10:49
+    115 | B0004   | http://wings.msn.to/neko/ | 192.168.70.166  | 2013-01-01 12:25:12
+    120 | C0002   | http://wings.msn.to/neko/ | 192.168.30.206  | 2013-01-02 14:11:17
+    125 | C0005   | http://wings.msn.to/neko/ | 192.168.211.109 | 2013-01-05 17:18:19
+    124 | C0004   | http://wings.msn.to/kame/ | 192.168.206.132 | 2013-01-05 13:16:12
+    118 | C0001   | http://wings.msn.to/hamu/ | 192.168.32.14   | 2013-01-01 23:18:14
+    121 | A0001   | http://wings.msn.to/hamu/ | 192.168.195.112 | 2013-01-03 04:10:49
+    126 | C0001   | http://wings.msn.to/hamu/ | 192.168.165.19  | 2013-01-05 20:18:19
+    123 | A0001   | http://wings.msn.to/hamu/ | 192.168.16.198  | 2013-01-05 11:16:12
+    119 | A0001   | http://wings.msn.to/hamu/ | 192.168.16.115  | 2013-01-02 07:11:17
+    117 | A0001   | http://wings.msn.to/hamu/ | 192.168.13.16   | 2013-01-01 22:25:12
+*/
